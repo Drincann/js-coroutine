2bf1a0f871188c87cfad00f3dfbf3a39a48378ef
drincann <1019933576@qq.com>
2022-06-30 21:19:02 +0800

    test: 增加测试

---
+++ test.ts
@@ -0,0 +1,117 @@
+import { Coroutine, MPromise } from "./src";
+
+// test cases
+class TestPromise {
+  // test case 1
+  public async test1() {
+    let o = {};
+    let result = await new Promise((resolve, reject) => {
+      try {
+        new MPromise(() => {
+          setTimeout(() => {
+            try {
+              resolve(o);
+            } catch (e) {
+              reject(e);
+            }
+          }, 0);
+        });
+      } catch (e) {
+        reject(e);
+      }
+    });
+    if (result !== o) {
+      throw { message: "test1" };
+    }
+  }
+
+  // test case 2
+  public async test2() {
+    let o = {};
+    const result = await new Promise((resolve, reject) => {
+      new MPromise(() => {
+        throw o;
+      }).then(undefined, error => {
+        resolve(error);
+      });
+    });
+    if (result !== o) {
+      throw { message: "test2" };
+    }
+  }
+
+  public static async run() {
+    for (let method of Object.getOwnPropertyNames(TestPromise.prototype)) {
+      if (method.startsWith("test")) {
+        try {
+          await (TestPromise.prototype as any)[method]();
+          console.log(`${method} passed`);
+        } catch (error) {
+          console.log(`${method} failed, error: ${JSON.stringify(error)}`);
+          continue;
+        }
+
+      }
+    }
+  }
+}
+
+class TestCoroutine {
+
+  public async test1() {
+    let o = {};
+    let result = await new Coroutine((function* () {
+      return o;
+    })()).execute();
+    if (result !== o) {
+      throw { message: "test1" };
+    }
+  }
+
+  public async test2() {
+    let o = {};
+    try {
+      let result = await new Coroutine((function* () {
+        throw o;
+      })()).execute();
+    } catch (error) {
+      if (error !== o) {
+        throw { message: "test2" };
+      }
+    }
+  }
+
+  public static async run() {
+    for (let method of Object.getOwnPropertyNames(TestCoroutine.prototype)) {
+      if (method.startsWith("test")) {
+        try {
+          await (TestCoroutine.prototype as any)[method]();
+          console.log(`${TestCoroutine.name} ${method} passed`);
+        } catch (error) {
+          console.log(`${method} failed, error: ${JSON.stringify(error)}`);
+          continue;
+        }
+
+      }
+    }
+  }
+}
+
+async function TestCasesClassRunner(TestClass: any) {
+  for (let method of Object.getOwnPropertyNames(TestClass.prototype)) {
+    if (method.startsWith("test")) {
+      try {
+        await (TestClass.prototype as any)[method]();
+        console.log(`${TestClass.name}.${method} passed`);
+      } catch (error) {
+        console.log(`${TestClass.name}.${method} failed, error: ${JSON.stringify(error)}`);
+        continue;
+      }
+    }
+  }
+}
+
+(async () => {
+  await TestCasesClassRunner(TestPromise);
+  await TestCasesClassRunner(TestCoroutine);
+})()
