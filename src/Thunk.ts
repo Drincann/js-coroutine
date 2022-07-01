2cdaeb1e6b014739e1bcda8ba0d93abe16a20376
drincann <1019933576@qq.com>
2022-07-01 12:03:36 +0800

    feature: 提供 Thunk 封装

---
+++ src/Thunk.ts
@@ -0,0 +1,22 @@
+export type ThunkFn<T = any> = (callback: (error: any, value: T) => void) => void;
+export type ThunkCallback<T = any> = (error: any, value: T) => void;
+
+export class Thunk<T = any> {
+  public static toThunk<T>(
+    asyncFn: (
+      ...argsAndCallback: (any | ThunkCallback<T>)[]
+    ) => void
+  ): (...args: any[]) => Thunk<T> {
+    return (...args: any[]): Thunk<T> => {
+      return new Thunk((callback: ThunkCallback<T>) => {
+        asyncFn(...args, callback);
+      });
+    }
+  }
+
+  public static isThunk(value: any): value is Thunk {
+    return value instanceof Thunk;
+  }
+
+  private constructor(public thunk: ThunkFn<T>) { }
+}
