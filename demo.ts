ae1c370def2e029ce5d6466dc291c98d4e576904
drincann <1019933576@qq.com>
2022-07-01 12:07:19 +0800

    feature: demo 中提供 Thunk 异步调度的示例

--- demo.ts
+++ demo.ts
@@ -1,21 +1,31 @@
 import { Coroutine, MPromise } from "./src";
-import fs from "fs";
+import { Thunk, ThunkCallback } from "./src/Thunk";
 
-function timer(ms: number, message: string): MPromise<string> {
+function timerPromise(ms: number, message: string): MPromise<string> {
   return new MPromise((resolve, _) => {
     setTimeout(() => {
-      resolve(`${new Date().toLocaleString()}> ${message} after ${ms}ms`);
+      resolve(`${new Date().toLocaleString()}> promise timer ${message} after ${ms}ms`);
     }, ms);
   });
 }
 
+const timerThunk = Thunk.toThunk((ms: number, message: string, callback: ThunkCallback): void => {
+  setTimeout(() => {
+    callback(null, `${new Date().toLocaleString()}> thunk timer ${message} after ${ms}ms`);
+  }, ms);
+})
+
 function* setMultiTimerSync(tasksOpt: Array<{ ms: number, message: string }>): Generator<any, string, any> {
   for (const task of tasksOpt) {
-    const result = yield timer(task.ms, task.message);
-    console.log(result);
+    const result1 = yield timerPromise(task.ms, task.message);
+    console.log(result1);
+    const result2 = yield timerThunk(task.ms, task.message);
+    console.log(result2);
   }
   return 'coroutine done';
 }
+
+
 
 new Coroutine(setMultiTimerSync([
   { ms: 1000, message: 'timer1' },
