c299c4d9cce5dff82f803715bfc79a3a50efeb8b
drincann <1019933576@qq.com>
2022-07-07 16:22:26 +0800

    feature(Coroutine): 现在 Coroutine 实例本身是 MPromise 的子类

--- src/GeneratorExecutor.ts
+++ src/GeneratorExecutor.ts
@@ -1,5 +1,6 @@
-import { MPromise, Thenable } from ".";
-import { Thunk } from ".";
+import { Thenable } from "./Promise";
+import { MPromise } from "./Promise";
+import { Thunk } from "./Thunk";
 
 function isGenerator(value: any): value is Generator<any, any, any> {
   return typeof value?.next === 'function' && typeof value?.throw === 'function';
@@ -9,47 +10,44 @@ function isThenable(value: any): value is Thenable<any> {
   return typeof value?.then === 'function';
 }
 
-export class Coroutine<T> {
+export class Coroutine<T> extends MPromise<T> {
   static isCoroutine(value: any): value is Coroutine<any> {
     return value instanceof Coroutine;
   }
 
-  private coroutine: Generator<any, T, any>;
-  public constructor(gen: Generator<any, T, any>) {
-    this.coroutine = gen;
+  public constructor(private coroutine: Generator<any, T, any>) {
+    super((resolve, reject) => this.execute(resolve, reject) /* use this outside the constructor */);
   }
 
-  execute(): MPromise<T> {
-    return new MPromise((resolve, reject) => {
-      const next = (value?: any) => {
-        const result = this.coroutine.next(value);
-        if (result.done) {
-          resolve(result.value);
+  private execute(resolve: (value: T) => void, reject: (error: any) => void): void {
+    const next = (value?: any) => {
+      const result = this.coroutine.next(value);
+      if (result.done) {
+        resolve(result.value);
+      } else {
+        if (isThenable(result.value)) {
+          // promise instance
+          result.value.then(next, reject);
+        } else if (isGenerator(result.value)) {
+          // generator
+          new Coroutine(result.value).then(next, reject);
+        } else if (Coroutine.isCoroutine(result.value)) {
+          // coroutine instance
+          result.value.then(next, reject);
+        } else if (Thunk.isThunk(result.value)) {
+          // thunk instance
+          result.value.thunk((err, value) => {
+            if (err) {
+              reject(err);
+            } else {
+              next(value);
+            }
+          })
         } else {
-          if (isThenable(result.value)) {
-            // promise instance
-            result.value.then(next, reject);
-          } else if (isGenerator(result.value)) {
-            // generator
-            new Coroutine(result.value).execute().then(next, reject);
-          } else if (Coroutine.isCoroutine(result.value)) {
-            // coroutine instance
-            result.value.execute().then(next, reject);
-          } else if (Thunk.isThunk(result.value)) {
-            // thunk instance
-            result.value.thunk((err, value) => {
-              if (err) {
-                reject(err);
-              } else {
-                next(value);
-              }
-            })
-          } else {
-            // value
-            next(result.value);
-          }
+          // value
+          next(result.value);
         }
-      }; next();
-    });
+      }
+    }; next();
   }
 }
